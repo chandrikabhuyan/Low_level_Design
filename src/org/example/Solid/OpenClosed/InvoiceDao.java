117a0581aa456f0672c34a20ca3fa3dd7f83c495
Chandrika Bhuyan <chandrika@Chandrikas-MacBook-Air.local>
2023-09-11 21:41:30 +0530

    Initial commit

---
+++ src/org/example/Solid/OpenClosed/InvoiceDao.java
@@ -0,0 +1,5 @@
+package org.example.Solid.OpenClosed;
+
+public class InvoiceDao {
+
+}
---
+++ src/org/example/Solid/SingleResponsibility/Marker.java
@@ -0,0 +1,72 @@
+package org.example.Solid.SingleResponsibility;
+
+public class Marker {
+    private String name;
+    private String color;
+    int price;
+    public Marker(String name,String color,int price) {
+        this.name = name;
+        this.color = color;
+        this.price = price;
+    }
+}
+
+class Invoice{
+    private Marker marker;
+    private int quantity;
+    public Invoice(Marker marker,int quantity) {
+        this.marker = marker;
+        this.quantity = quantity;
+    }
+    public int calculateTotal() {
+        int total = marker.price * quantity;
+        return total;
+    }
+    public void printInvoice(){
+        //implement
+    }
+
+    public void saveToDB(){
+        //implement
+    }
+}
+
+// The `Invoice' class is about invoices, but we have added print and storage functionality inside it.
+// This breaks the SRP rule, which states, "A class should have only one reason to change."
+// If we want to change the calculation, logic of the printing or storage functionality in the future, we would need to change the class.
+
+
+class InvoiceCal{
+    private Marker marker;
+    private int quantity;
+    public InvoiceCal(Marker marker,int quantity) {
+        this.marker = marker;
+        this.quantity = quantity;
+    }
+    public int calculateTotal() {
+        int total = marker.price * quantity;
+        return total;
+    }
+}
+
+class InvoiceDao{
+    private InvoiceCal invoiceCal;
+    public InvoiceDao(InvoiceCal invoiceCal) {
+        this.invoiceCal = invoiceCal;
+    }
+
+    public void saveToDB() {
+        //save to DB
+    }
+}
+
+class InvoicePrint{
+    private InvoiceCal invoiceCal;
+    public InvoicePrint(InvoiceCal invoiceCal){
+        this.invoiceCal = invoiceCal;
+    }
+    public void printInvoice(){
+        //print
+    }
+}
+
